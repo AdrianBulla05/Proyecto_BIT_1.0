f4a36128465c82f73d619652895dc57e9af15ab1
AdrianBulla05 <abullabe@ibero.edu.co>
2023-03-30 17:00:45 -0500

    Se crea la pestaña de Login del proyecto

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-var a;
+/* Función para visualizar la alternancia de los formularios de inicio de sesión o registro */
+const $btnSignIn= document.querySelector('.btn-sign-in'), 
+$btnSignUp= document.querySelector('.btn-sign-up'),
+$signUp= document.querySelector('.sign-up'),
+$signIn= document.querySelector('.sign-in')
+
+
+    document.addEventListener('click', e => {
+        if (e.target === $btnSignIn || e.target === $btnSignUp) {
+            $signIn.classList.toggle('active');
+            $signUp.classList.toggle('active')
+        }
+    })
